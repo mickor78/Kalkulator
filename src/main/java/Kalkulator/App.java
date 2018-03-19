41888c5ad146f5a6bfad81abb1620cf6204a4f08
michal <mkorzeni@mion.pw.edu.pl>
2018-03-19 21:30:25 +0100

    init

---
+++ src/main/java/Kalkulator/App.java
@@ -0,0 +1,20 @@
+package Kalkulator;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    public static double add(double a, double b) {
+        return a + b;
+    }
+
+    public static double delta(double a, double b, double c) {
+        return b * b - 4 * a * c;
+    }
+
+
+}
