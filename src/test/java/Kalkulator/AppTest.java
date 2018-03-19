5b611697b5b56da8f6b76765e15e78eada210d28
michal <mkorzeni@mion.pw.edu.pl>
2018-03-19 23:04:42 +0100

    Make project lifecycle install. In pom added plugins making .jar executable - not work Error: /home/michal/jdk-9.0.4/bin/java -Dfile.encoding=UTF-8 -jar /home/michal/IdeaProjects/Kalkulator/target/Kalkulator-1.0-SNAPSHOT.jar Error: Could not find or load main class App Caused by: java.lang.ClassNotFoundException: App

--- src/test/java/Kalkulator/AppTest.java
+++ src/test/java/Kalkulator/AppTest.java
@@ -1,56 +1,60 @@
-package Kalkulator;
-
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
-
-    @org.junit.jupiter.api.Test
-    void addTwo() {
-        //given
-        //when
-        double result=App.addTwo(2,2);
-        //then
-        assertEquals(6, result);
-    }
-
-    @org.junit.jupiter.api.Test
-    void delta() {
-        //given
-        //when
-        double delta = App.delta(2, 2, 2);
-        //then
-        assertEquals(-12,delta);
-    }
-}
+//package Kalkulator;
+//
+//import junit.framework.Test;
+//import junit.framework.TestCase;
+//import junit.framework.TestSuite;
+//
+//*
+// * Unit test for simple App.
+//
+//
+//public class AppTest
+//    extends TestCase
+//{
+//*
+//     * Create the test case
+//     *
+//     * @param testName name of the test case
+//
+//
+//    public AppTest( String testName )
+//    {
+//        super( testName );
+//    }
+//
+//*
+//     * @return the suite of tests being tested
+//
+//
+//    public static Test suite()
+//    {
+//        return new TestSuite( AppTest.class );
+//    }
+//
+//*
+//     * Rigourous Test :-)
+//
+//
+//    public void testApp()
+//    {
+//        assertTrue( true );
+//    }
+//
+//    @org.junit.jupiter.api.Test
+//    void addTwo() {
+//        //given
+//        //when
+//        double result=App.addTwo(2,2);
+//        //then
+//        assertEquals(6, result);
+//    }
+//
+//    @org.junit.jupiter.api.Test
+//    void delta() {
+//        //given
+//        //when
+//        double delta = App.delta(2, 2, 2);
+//        //then
+//        assertEquals(-12,delta);
+//    }
+//}
